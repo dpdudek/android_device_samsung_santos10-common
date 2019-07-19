59509bf9c2015f947fab7f1c436609ee0815ba81
Dariusz Dudek <dariusz.dudek@gmail.com>
2019-07-19 21:04:28 +0200

    move SHARED_LIBRARIES to vendor
    Change-Id: I64b26a07af12c8c381bc6ba3f942437ab8e84993

--- hdmi_helper/Android.mk
+++ hdmi_helper/Android.mk
@@ -1,21 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE := libmultidisplay
-LOCAL_SRC_FILES := libmultidisplay.so # <== Moved the prebuilt lib to root / -- oddly enough, when it was in a subfolder, make would not find it.
-LOCAL_MODULE_TAGS := debug
-LOCAL_EXPORT_C_INCLUDES := vendor/samsung/santos10-common/proprietary/vendor/lib
-
-include $(BUILD_PREBUILT)
-
-
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := hdmi_helper.cpp
-LOCAL_LDFLAGS := -L vendor/samsung/santos10-common/proprietary/vendor/lib
+#LOCAL_LDFLAGS := -L vendor/samsung/santos10-common/proprietary/vendor/lib
 LOCAL_LDLIBS := -lmultidisplay
 LOCAL_SHARED_LIBRARIES += libbinder liblog libutils
 LOCAL_MODULE_PATH := $(TARGET_OUT)/bin
