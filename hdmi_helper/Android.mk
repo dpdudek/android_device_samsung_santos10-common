2083f79ac346b0b2e17de08659fd92bf76d669c7
Dariusz Dudek <dariusz.dudek@gmail.com>
2019-07-19 20:17:52 +0200

    fix missing library error
    Change-Id: Ib7fe06a0c3a1b70bd4b1914345b3fdd3c4cae07c

--- hdmi_helper/Android.mk
+++ hdmi_helper/Android.mk
@@ -1,5 +1,17 @@
 LOCAL_PATH := $(call my-dir)
 
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE := libmultidisplay
+LOCAL_SRC_FILES := libmultidisplay.so # <== Moved the prebuilt lib to root / -- oddly enough, when it was in a subfolder, make would not find it.
+LOCAL_MODULE_TAGS := debug
+LOCAL_EXPORT_C_INCLUDES := vendor/samsung/santos10-common/proprietary/vendor/lib
+
+include $(BUILD_PREBUILT)
+
+
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := hdmi_helper.cpp
--- pvrsrvctl/Android.mk
+++ pvrsrvctl/Android.mk
@@ -2,6 +2,26 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE := libsrv_init
+LOCAL_SRC_FILES := libsrv_init.so # <== Moved the prebuilt lib to root / -- oddly enough, when it was in a subfolder, make would not find it.
+LOCAL_MODULE_TAGS := debug
+LOCAL_EXPORT_C_INCLUDES := vendor/samsung/santos10-common/proprietary/vendor/lib
+
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE := libsrv_um
+LOCAL_SRC_FILES := libsrv_um.so # <== Moved the prebuilt lib to root / -- oddly enough, when it was in a subfolder, make would not find it.
+LOCAL_MODULE_TAGS := debug
+LOCAL_EXPORT_C_INCLUDES := vendor/samsung/santos10-common/proprietary/vendor/lib
+
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+
 LOCAL_SRC_FILES := pvrsrvctl.c
 LOCAL_LDFLAGS := -L vendor/samsung/santos10-common/proprietary/vendor/lib
 LOCAL_LDLIBS := -lsrv_init -lsrv_um
