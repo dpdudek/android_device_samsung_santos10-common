b55e2e847e9c7a3a0894ccdadb0fa20dc8807957
Dariusz Dudek <dariusz.dudek@gmail.com>
2019-07-20 21:43:21 +0200

    fix LDLIBS
    Change-Id: I1e3ab102f6d57e77e0e6bf59357c09357ea00204

--- hdmi_helper/Android.mk
+++ hdmi_helper/Android.mk
@@ -3,7 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := hdmi_helper.cpp
-#LOCAL_LDFLAGS := -L vendor/samsung/santos10-common/proprietary/vendor/lib
 LOCAL_LDLIBS := -lmultidisplay
 LOCAL_SHARED_LIBRARIES += libbinder liblog libutils
 LOCAL_MODULE_PATH := $(TARGET_OUT)/bin
